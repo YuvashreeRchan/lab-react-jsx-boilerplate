5c1062b44cfe459c79eb4d008da89a3f5dfdd01f
YuvashreeRchan <popsicle2805@gmail.com>
2024-04-23 17:19:22 +0530

    completed

--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -27,4 +27,19 @@ export default class AppClass extends Component{
   }
 
   // code here
+  render(){
+    const images = this.imageData();
+    return (
+      <div className="gallery-box">
+        <h1>Kalvium Gallery</h1>
+        <div className="image-row">
+          {images.map(image => (
+            <div>
+              <img src={image.img} alt={`Image ${image.id}`} className="full-size"/>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }

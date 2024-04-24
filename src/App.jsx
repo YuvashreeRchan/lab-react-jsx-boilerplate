5e738823a7b34d441b87c66ca5e5f6d5514360c7
YuvashreeRchan <popsicle2805@gmail.com>
2024-04-24 19:30:46 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
 import './App.css';
-import imageData from './components/DataComponent';
+import elephant from "./images/elephant.jpeg";
 
+const imageData = ()=>{
+  let data = [
+    {
+      id:1,
+      img:elephant
+    },
+    {
+      id:2,
+      img:elephant
+    },
+    {
+      id:3,
+      img:elephant
+    },
+    {
+      id:4,
+      img:elephant
+    }
+  ]
+  return data;
+}
 
 function App() {
   // code here

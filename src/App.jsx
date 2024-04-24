2ca28e344c45b46df994fd4f84034e50ee41e757
YuvashreeRchan <popsicle2805@gmail.com>
2024-04-24 19:18:04 +0530

    added props

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,6 @@
 import './App.css';
-import elephant from "./images/elephant.jpeg";
+import imageData from './components/DataComponent';
 
-const imageData = ()=>{
-  let data = [
-    {
-      id:1,
-      img:elephant
-    },
-    {
-      id:2,
-      img:elephant
-    },
-    {
-      id:3,
-      img:elephant
-    },
-    {
-      id:4,
-      img:elephant
-    }
-  ]
-  return data;
-}
 
 function App() {
   // code here
--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -1,39 +1,16 @@
 import { Component } from "react";
 import "./App.css"
-import elephant from "./images/elephant.jpeg";
+import imageData from './components/DataComponent';
 
 export default class AppClass extends Component{
   
-  imageData = ()=>{
-    let data = [
-      {
-        id:1,
-        img:elephant
-      },
-      {
-        id:2,
-        img:elephant
-      },
-      {
-        id:3,
-        img:elephant
-      },
-      {
-        id:4,
-        img:elephant
-      }
-    ]
-    return data;
-  }
-
   // code here
   render(){
-    const images = this.imageData();
     return (
       <div className="gallery-box">
         <h1>Kalvium Gallery</h1>
         <div className="image-row">
-          {images.map(image => (
+          {imageData().map(image => (
             <div>
               <img src={image.img} alt={`Image ${image.id}`} className="full-size"/>
             </div>
